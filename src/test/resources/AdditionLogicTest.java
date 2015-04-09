41a8a8088a131c37ade1cbc2d8389d5100308d04
r.b.mallikarjunappa <r.b.mallikarjunappa@BDC7-L-3NPX8R.GDNINDIA.COM>
2015-04-09 19:12:08 +0530

    eclipse project

---
+++ src/test/resources/AdditionLogicTest.java
@@ -0,0 +1,17 @@
+package test.devops.servlet;
+
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+public class AdditionLogicTest extends TestCase {
+	@Test	
+	public final void testAddition() {
+		AdditionLogic al=new AdditionLogic();
+		assertEquals("10+30 must be 40",40, al.addition(10,30));
+		assertEquals("30+30 must be 60",60, al.addition(30,30));
+		assertEquals("0+30 must be 30",30, al.addition(0,30));	
+		
+	}
+
+}
