79793b37928709f62e156e6f042a88089bd502ba
r.b.mallikarjunappa <r.b.mallikarjunappa@BDC7-L-3NPX8R.GDNINDIA.COM>
2015-04-14 16:52:44 +0530

    Changes to unit test class

--- src/test/resources/AdditionLogicTest.java
+++ src/test/resources/AdditionLogicTest.java
@@ -8,7 +8,7 @@ public class AdditionLogicTest extends TestCase {
 	@Test	
 	public final void testAddition() {
 		AdditionLogic al=new AdditionLogic();
-		assertEquals("10+30 must be 40",40, al.addition(10,30));
+		assertEquals("10+30 must be 40",20, al.addition(10,30));
 		assertEquals("30+30 must be 60",60, al.addition(30,30));
 		assertEquals("0+30 must be 30",30, al.addition(0,30));	
 		
